179d40404a4ec3edd91f1185732fa56d504bdfbe
Mopp <twoandahalfmopp@gmail.com>
2026-05-09 10:39:24 -0400

    Jank Fix for the vanilla 4-tick use delay messing with gun firing stuff. I didn't actually fix it, I just noted down that firing interval always needs to be divisible by 4.

--- src/main/java/net/moppzarella/mzguns/item/custom/GunItem.java
+++ src/main/java/net/moppzarella/mzguns/item/custom/GunItem.java
@@ -7,8 +7,10 @@ import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResultHolder;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.AirItem;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
 import net.minecraft.world.level.Level;
 import net.moppzarella.mzguns.Config;
 import net.moppzarella.mzguns.MZGuns;
@@ -20,7 +22,7 @@ public class GunItem extends Item {
 
     public final float bloomRadius = 0.7F; //I don't know if this is measured in degrees or radians
     public final double bloomDecayRate = 0.04D; //The amount to subtract from current_bloom (if it's above 0) each tick
-    public final int firingRate = 10; //The amount of ticks between shots
+    public final int firingRate = 12; //The amount of ticks between shots. Must be divisible by 4
     public final int clipSize = 6;
     public final int clipReloadSpeed = 23;
     public final int clipReloadAmount = 6;
@@ -29,24 +31,15 @@ public class GunItem extends Item {
         super(properties);
     }
 
-    public void onAttackOrUse(Level level, Player player, InteractionHand usedHand, ItemStack stack) {
-        if (!level.isClientSide) {
-            switch (usedHand) {
-                case MAIN_HAND: {
-                    if(player.getMainHandItem() == stack) {
-                        primaryFire(level, player, usedHand);
-                    }
-                }
-                case OFF_HAND: {
-                    if(player.getOffhandItem() == stack) {
-                        MZGuns.LOGGER.info("offhandItem");
-                        primaryFire(level, player, usedHand);
-                    }
-                }
+    @Override
+    public InteractionResultHolder<ItemStack> use(Level level, Player player, InteractionHand usedHand) {
+            if (!level.isClientSide && usedHand == InteractionHand.OFF_HAND){
+                primaryFire(level, player, usedHand);
+                return InteractionResultHolder.pass(player.getItemInHand(usedHand));
             }
-        }
-    }
+            return InteractionResultHolder.fail(player.getItemInHand(usedHand));
 
+    }
     public void primaryFire(Level level, Player player, InteractionHand usedHand) {
         ItemStack stackInHand = player.getItemInHand(usedHand);
         if(!level.isClientSide) {
@@ -103,7 +96,7 @@ public class GunItem extends Item {
                 this.setFiringCooldown(stack, firing_cooldown - 1);
             }
 
-            if ((getCurrentClip(stack) < clipSize) && this.getFiringCooldown(stack) <= 0D && !this.isOnFiringCooldown(stack)) {
+            if ((getCurrentClip(stack) < clipSize) && this.getFiringCooldown(stack) <= 0D && !this.isOnFiringCooldown(stack) && (((Player) entity).getMainHandItem() == stack || ((Player) entity).getOffhandItem() == stack)) {
                 int reloadProgress = getReloadProgress(stack);
                 if (reloadProgress >= clipReloadSpeed) {
                     setReloadProgress(stack, 0);
@@ -112,6 +105,8 @@ public class GunItem extends Item {
                     setCurrentClip(stack, clipToAdd);
                 } else {
                     setReloadProgress(stack, reloadProgress + 1);}
+            } else if (getReloadProgress(stack) != 0 && !(((Player) entity).getMainHandItem() == stack || ((Player) entity).getOffhandItem() == stack)) {
+                setReloadProgress(stack, 0);
             }
 
         }
